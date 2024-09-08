bcc8d3cce019737f618b1aa6fe41f38fbd312edb
Acutapugione <acuta.pugione@gmail.com>
2024-09-08 22:17:39 +0300

    ,,,

--- flask_blog/init_db.py
+++ flask_blog/init_db.py
@@ -1,21 +1,21 @@
 import sqlite3
 
-connection = sqlite3.connect('database.db')
+connection = sqlite3.connect("flask_blog/database.db")
 
 
-with open('schema.sql') as f:
-	connection.executescript(f.read())
+with open("flask_blog/schema.sql") as f:
+    connection.executescript(f.read())
 
 
 cur = connection.cursor()
 
-#cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-#('First Post', 'Content for the first post')
-#)
+# cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+# ('First Post', 'Content for the first post')
+# )
 
-#cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-#('Second Post', 'Content for the second post')
-#)
+# cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+# ('Second Post', 'Content for the second post')
+# )
 
 connection.commit()
 connection.close()
